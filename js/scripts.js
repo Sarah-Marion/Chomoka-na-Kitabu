789918a23a6dc9fdd7efd21cc93ae14fd78c0639
Sarah-Marion <devsarahmarion@gmail.com>
2018-04-10 14:35:50 +0300

    basic page layout for website

---
+++ js/scripts.js
@@ -0,0 +1,85 @@
+// var indexPos = 0;
+
+// function bookSlides() {
+//     var nextSlide = document.getElementsByClassName("book-slides");
+//     for (var i = 0; i < nextSlide.length; i++) {
+//         nextSlide[i].style.display = "none";
+//     }
+//     indexPos++;
+//     if (indexPos > nextSlide.length) {
+//         indexPos = 1
+//     }
+//     nextSlide[indexPos - 1].style.display = "block";
+//     setTimeout(bookSlides, 2000);
+// }
+var slideIndex = 1;
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+$(document).ready(function () {
+    // This facilitates smooth scrolling using jQuery easing
+    $('a.smooth-scroll[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 48)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+    // Closes responsive menu when a smooth scroll class is triggered
+    // this is for small screens with small pixel widths
+    $('.smooth-scroll').click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+    //Scroll to the top
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#navMain',
+        offset: 54
+    });
+    // Collapse Navbar when the scroll is triggered
+    var navbarCollapse = function () {
+        if ($("#navMain").offset().top > 100) {
+            $("#navMain").addClass("navbar-shrink");
+        } else {
+            $("#navMain").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse the nav bar if page is not at the top
+    navbarCollapse();
+    // Collapse the navbar when there is scroll activity
+    $(window).scroll(navbarCollapse);
+    showSlides(slideIndex);
+    // bookSlides();
+    showSlides(slideIndex);
+});
